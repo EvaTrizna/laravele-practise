c54734c8e1185b997ba0fbc419270beb79bd6bcb
Eva Trizna <eva.trizna@mail.ru>
2024-04-08 15:00:38 +0300

    feat: email notifications when creating a new chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Chirp extends Model
 {
     use HasFactory;
@@ -13,6 +14,9 @@ class Chirp extends Model
         'message',
     ];
 
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
